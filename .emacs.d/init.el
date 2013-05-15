d6e43d2ad35d8cd1d02a5e9cf99571d4508f662c
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-05-15 16:22:22 -0400

    fixup! Set up separate directory for my El-Get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
   "The name of the file that holds my customizations.")
 ;; Other possible names are `customizations.el',
 
-(defconst +EL-GET_RECIPES_DIRECTORY+ "recipes for el-get"
+(defconst +EL-GET_RECIPES_DIRECTORY+ "Recipes for El-Get"
   "The name of the directory that holds my local el-get `.rcp' files.")
 
 ;; (Common Lisp has the convention of bookending names of constants with `+'.)
