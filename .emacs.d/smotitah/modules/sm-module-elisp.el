4ed5eea20e40a4a6c07b335fe6bef90546aaaa38
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-05-23 17:04:54 -0400

    Restrain `elisp-slime-nav` for cleaner modeline.
    (I also installed Diminish version 0.44)

--- .emacs.d/smotitah/modules/sm-module-elisp.el
+++ .emacs.d/smotitah/modules/sm-module-elisp.el
@@ -15,6 +15,12 @@
   ;; Enable SLIME-like navigation with M-. and M-,
   (add-hook 'emacs-lisp-mode-hook #'elisp-slime-nav-mode)
   (add-hook 'ielm-mode-hook       #'elisp-slime-nav-mode)
+
+  ;; Since I want `elisp-slime-nav-mode' to be on whenever
+  ;; I'm doing _anything_ in Emacs Lisp, its making itself
+  ;; known in the modeline is unnecessary.
+  (eval-after-load 'elisp-slime-nav
+    '(diminish 'elisp-slime-nav-mode))
   
   ;; for some reason, the `prog-mode' keybindings aren't coming through...
   (bind-key "C-j" #'newline emacs-lisp-mode-map)
