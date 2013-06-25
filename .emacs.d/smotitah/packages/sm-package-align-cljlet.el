2f04ea2faa3ad9f031a87974101123a2d3ccf9ba
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-06-25 15:02:24 -0400

    Use paredit & align-cljlet as a Clojurian.
    Install paredit version 24 (beta) and align-cljlet version 0.3.

---
+++ .emacs.d/smotitah/packages/sm-package-align-cljlet.el
@@ -0,0 +1,8 @@
+;;;; Package align-cljlet
+(sm-package align-cljlet
+            ;;:package-manager "el-get"
+            :unmanaged-p t)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package align-cljlet)
---
+++ .emacs.d/smotitah/packages/sm-package-paredit.el
@@ -0,0 +1,8 @@
+;;;; Package paredit
+(sm-package paredit
+            ;;:package-manager "el-get"
+            :unmanaged-p t)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package paredit)
