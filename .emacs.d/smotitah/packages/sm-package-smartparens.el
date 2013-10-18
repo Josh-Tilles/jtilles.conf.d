e7b2cfb5fd36107b7093e46d8a9ff06c1d250863
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-10-18 16:01:38 -0400

    Install Emacs’s Smartparens version "1.5".
    I grabbed the `smartparens.rcp` file from the `master` branch of
    `el-get`.

---
+++ .emacs.d/smotitah/packages/sm-package-smartparens.el
@@ -0,0 +1,8 @@
+;;;; Package smartparens
+(sm-package smartparens
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package smartparens)
