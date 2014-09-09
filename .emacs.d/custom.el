2df761f08031e0c1d29a1fc715e29af123358656
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-09-09 03:05:24 -0400

    revert! "Emacs, ensure all scripty files are exec-able."
    Kinda reverts d275cecb90072755debd9f91011ec9e1524394e5

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,9 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(after-save-hook
-   (quote
-    (executable-make-buffer-file-executable-if-script-p)))
  '(apropos-do-all t)
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
  '(blink-matching-paren nil nil nil "I don't like the cursor jumping around on me.")
