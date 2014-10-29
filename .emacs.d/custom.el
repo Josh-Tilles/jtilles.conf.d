c144f262b9df61bd116c750e4205221a3ee2b0b6
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-10-29 17:03:06 -0400

    revert! In Emacs, don't blink the match of a close-paren.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
- '(blink-matching-paren nil nil nil "I don't like the cursor jumping around on me.")
  '(comment-auto-fill-only-comments t)
  '(custom-safe-themes
    (quote
